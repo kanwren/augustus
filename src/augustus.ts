fd009122ce908d859f78a7a65b7cb7b1c390026c
nprindle <nprindle18@gmail.com>
2020-05-22 22:54:46 -0400

    Export InjectSchema

--- src/augustus.ts
+++ src/augustus.ts
@@ -1,2 +1,2 @@
-export { Schema, DomainOf, ReprOf, Schemas, LazySchemas } from "./Schema";
+export { Schema, InjectSchema, DomainOf, ReprOf, Schemas, LazySchemas } from "./Schema";
 export { jsonEncodeWith, DecodeResult, jsonDecodeWith } from "./Serialize";
